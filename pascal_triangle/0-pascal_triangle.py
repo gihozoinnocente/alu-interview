83eb554b556d6e93283d1e77141b1a50f35d4dc3
GIHOZO Innocente <i.gihozo@alustudent.com>
2024-02-23 13:14:11 +0000

    Pascal triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def printPascal(n):
+
+    for i in range(n+1):
+        c=1
+        for j in range(1, i+1):
+            print(c, sep=' ', end='')
+            c=c*(i-j)//j
+        print()
+#driver code
+n = int(input("num: "))
+printPascal(n)
