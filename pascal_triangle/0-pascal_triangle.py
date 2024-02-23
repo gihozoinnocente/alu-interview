d83151e8ef46a31939d06e248bc7f168da38ae3b
GIHOZO Innocente <i.gihozo@alustudent.com>
2024-02-23 13:22:08 +0000

    Documenting function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def printPascal(n):
+"""a function def pascal_triangle(n): that returns a list of lists of
+integers representing the Pascal's triangle of n:
 
+Returns an empty list if n <= 0
+You can assume n will be always an integer """
+def printPascal(n):
+    """Comment"""
     for i in range(n+1):
         c=1
         for j in range(1, i+1):
